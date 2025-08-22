2820cb867bb74bcfdccc851edc08af5fbc3a768b
razvan daraban <razvan.daraban@davidson.fr>
2025-08-22 13:35:43 +0200

    booking available only in future competitions

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import json
+from datetime import datetime
+
 from flask import Flask,render_template,request,redirect,flash,url_for
 
 
@@ -13,6 +15,15 @@ def loadCompetitions():
          listOfCompetitions = json.load(comps)['competitions']
          return listOfCompetitions
 
+def get_club_by_name(name):
+    return next((club for club in clubs if club['name'] == name), None)
+
+def get_competition_by_name(name):
+    return next((comp for comp in competitions if comp['name'] == name), None)
+
+def is_future_competition(comp):
+    comp_date = datetime.strptime(comp['date'], "%Y-%m-%d %H:%M:%S")
+    return comp_date >= datetime.now()
 
 app = Flask(__name__)
 app.secret_key = 'something_special'
@@ -31,14 +42,21 @@ def showSummary():
 
 
 @app.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
-    else:
-        flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+def book(competition, club):
+    comp = get_competition_by_name(competition)
+    club_data = get_club_by_name(club)
+
+    if not comp or not club_data:
+        flash("Error: Competition or club not found")
+        return redirect(url_for('index'))
+
+    # can not book past competitions
+    # Test: test_book_with_past_competition()
+    if not is_future_competition(comp):
+        flash("Error: Cannot book places for past competitions")
+        return redirect(url_for('showSummary'))
+
+    return render_template('booking.html', club=club_data, competition=comp)
 
 
 @app.route('/purchasePlaces',methods=['POST'])
